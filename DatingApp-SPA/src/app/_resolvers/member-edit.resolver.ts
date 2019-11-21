9c9a6d74a9d33cac1bc8418d901a13f5dbc53328
John Mikalauskas <johnmik35@hotmail.com>
2019-11-21 11:29:58 -0600

    Chap 10 - all working

--- DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -6,17 +6,19 @@ import { UserService } from '../_services/user.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
-export class MemberDetailResolver implements Resolve<User> {
+export class MemberEditResolver implements Resolve<User> {
 
-    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {
+    constructor(private userService: UserService, private authService: AuthService,
+         private router: Router, private alertify: AlertifyService) {
     }
 
     resolve(route: ActivatedRouteSnapshot): Observable<User> {
-        return this.userService.getUser(route.params.id).pipe(
+        return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
             catchError(error => {
-                this.alertify.error('Problem retrieving users in member-detail.resolver' + error);
+                this.alertify.error('Problem retrieving user in member-edit.resolver' + error);
                 this.router.navigate(['/members']);
                 return of(null);
              })
